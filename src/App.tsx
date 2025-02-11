cbd4011bfb6ab96e4d012d4e14bb048eb6bb628a
Lakshman Sai <lakshmansai121@gmail.com>
2025-02-11 19:24:41 +0530

    Init2

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { Button } from "@/components/ui/button"
+
 function App() {
-  return <div>app</div>
+  return (
+    <div>
+      app
+      <Button className="bg-red-500">Click me</Button>
+    </div>
+  )
 }
 
 export default App
